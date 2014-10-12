e84a41e27eb4e10d06a708c9d0ede03b806d5ad1
dbenoff <dbenoff@camsys.com>
2014-10-12 10:02:28 -0700

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,54 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../lib/require/text',
+        'durandal':'../lib/durandal/js',
+        'plugins' : '../lib/durandal/js/plugins',
+        'transitions' : '../lib/durandal/js/transitions',
+        'knockout': '../lib/knockout/knockout-3.1.0',
+        'bootstrap': '../lib/bootstrap/js/bootstrap',
+        'jquery': '../lib/jquery/jquery-1.9.1',
+        'jstree' : '../lib/jstree/jstree',
+        'jquery-ui' : '../lib/jquery-ui/jquery-ui',
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+       }
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'knockout'],  function (system, app, viewLocator, ko) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'Durandal Starter Kit';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+
+        ko.bindingHandlers.fadeVisible = {
+            init: function(element, valueAccessor) {
+                // Initially set the element to be instantly visible/hidden depending on the value
+                var value = valueAccessor();
+                $(element).toggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+            },
+            update: function(element, valueAccessor) {
+                // Whenever the value subsequently changes, slowly fade the element in or out
+                var value = valueAccessor();
+                ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
+            }
+        };
+    });
+});
---
+++ app/viewmodels/formtest.js
@@ -0,0 +1,128 @@
+﻿define(['plugins/http', 'durandal/app', 'knockout', 'jstree', 'bootstrap', 'jquery-ui'], function (http, app, ko, jstree, bootstrap) {
+
+    return {
+        displayName: 'formtest',
+
+        selectedGeographicType: ko.observable(),
+
+        geographicTypes: ko.observableArray([]),
+        useGeographicFilter: ko.observable(),
+        geoFilters: ko.observableArray([]),
+
+        assetTypes: ko.observableArray([]),
+        useAssetFilter: ko.observable(),
+        assetFilters: ko.observableArray([]),
+
+        geoTreeNodes: null,
+        assetTreeNodes: null,
+        assetTree: null,
+        geoTree: null,
+
+        activate: function () {
+            var that = this;
+            $.get( "querydata.json",
+                function(data) {
+                    var geoTreeNodes = [];
+                    $(data['GeographicTypes']).each(function () {
+                        var geoTypeName = Object.getOwnPropertyNames (this)[0];
+                        that.geographicTypes().push(geoTypeName);
+                        var geoTreeNode = {};
+                        geoTreeNode.children = [];
+                        geoTreeNode.text = geoTypeName;
+                        geoTreeNode.a_attr = {'selectionId' : geoTypeName},
+                        geoTreeNode.selectionId = geoTypeName;
+                        geoTreeNode.state = { 'opened' : false, 'selected' : false };
+                        that.geoTreeNode = geoTreeNode;
+                        var $this = $(this);
+                        $(this[geoTypeName]).each(function (index, childElement) {
+                            var child = {};
+                            child.text = childElement.Name;
+                            child.a_attr = {'selectionId' : childElement.Value},
+                            child.state = { 'opened' : false, 'selected' : false };
+                            child.parentId = that.geoTreeNode.selectionId;
+                            that.geoTreeNode.children.push(child);
+                        });
+
+                        geoTreeNodes.push(geoTreeNode);
+
+                    });
+                    that.geoTreeNodes = geoTreeNodes;
+
+                    var assetTreeNodes = [];
+                    $(data['AssetTypes']).each(function () {
+                        var assetTypeName = Object.getOwnPropertyNames (this)[0];
+                        var assetTreeNode = {};
+                        assetTreeNode.children = [];
+                        assetTreeNode.text = assetTypeName;
+                        assetTreeNode.a_attr = {'selectionId' : assetTypeName},
+                        assetTreeNode.selectionId = assetTypeName;
+                        assetTreeNode.state = { 'opened' : false, 'selected' : false };
+                        that.assetTreeNode = assetTreeNode;
+                        $(this[assetTypeName]).each(function (index, childElement) {
+                            var child = {};
+                            child.text = childElement.Name;
+                            child.a_attr = {'selectionId' : childElement.Value},
+                            child.state = { 'opened' : false, 'selected' : false };
+                            child.parentId = that.assetTreeNode.selectionId;
+                            that.assetTreeNode.children.push(child);
+                        });
+
+                        assetTreeNodes.push(assetTreeNode);
+
+                    });
+                    that.assetTreeNodes = assetTreeNodes;
+                }
+            );
+        },
+
+        attached: function (view, parent) {
+            var that =this;
+            $('#assetTree').jstree({'plugins':["checkbox"], 'core' : {
+                'data' : this.assetTreeNodes
+            }}).on('changed.jstree', function (e, data) {
+                that.updateAssetSelection(e, data)
+            });
+
+            $('#geoTree').jstree({'plugins':["checkbox"], 'core' : {
+                'data' : this.geoTreeNodes
+            }}).on('changed.jstree', function (e, data) {
+                that.updateGeoSelection(e, data)
+            });
+
+            this.useGeographicFilter.subscribe(function(newValue) {
+                var oldValue = that.useGeographicFilter();
+                if(oldValue){
+                    $("#geoTree").jstree("uncheck_all",true);
+                    that.useAssetFilter();
+                    that.assetFilters([]);
+                }
+            }, null, "beforeChange");
+
+            this.useAssetFilter.subscribe(function(newValue) {
+                var oldValue = that.useAssetFilter();
+                if(oldValue){
+                    $("#assetTree").jstree("uncheck_all",true);
+                }
+            }, null, "beforeChange");
+        },
+
+        updateGeoSelection: function (e, data) {
+            var values = $("#geoTree").jstree("get_checked",{full:true},true) ;
+            if(values.length > 0){
+                this.geoFilters(values);
+            }else{
+                this.geoFilters([]);
+            }
+        },
+
+        updateAssetSelection: function (e, data) {
+            var values = $("#assetTree").jstree("get_checked",{full:true},true) ;
+            if(values.length > 0){
+                this.assetFilters(values);
+            }else{
+                this.assetFilters([]);
+            }
+        },
+
+    };
+});
